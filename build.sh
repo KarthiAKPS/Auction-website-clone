90c6d8b4e06d7908e230e7529ad9927956c2841f
Karthi <karthiakps333@gmail.com>
2023-07-28 17:39:48 +0530

    changed the configs 6

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Building the project..."
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "colecting Static..."
+python3.9 manage.py collectstatic --noinput --clear
