62a464c74ef6819c12105ee4492e61b748bd6ffb
Karthi <karthiakps333@gmail.com>
2023-07-28 17:02:59 +0530

    changed the configs 1

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Building the project..."
-python3.11 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
