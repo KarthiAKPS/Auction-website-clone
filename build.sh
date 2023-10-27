a50820c24808700b1cd82d6c871075bae52c0827
Karthi <karthiakps333@gmail.com>
2023-10-27 16:50:44 +0530

    shell command changes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "colecting Static..."
 python3.9 manage.py collectstatic --no-input
