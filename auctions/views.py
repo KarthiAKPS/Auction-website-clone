6b1ec5e06c83d47ada9f200999794d2ae41d4596
Karthi <karthiakps333@gmail.com>
2023-11-17 16:25:31 +0530

    style changes

--- auctions/views.py
+++ auctions/views.py
@@ -293,9 +293,14 @@ def sell(request, id):
 def solditems(request):
     if request.method == "POST":
         cat = request.POST["category"]
-        c = Category.objects.get(category=cat)
+        
+        if cat != 'all':
+            c = Category.objects.get(category=cat)
+            items = listing.objects.filter(isActive=False, category=c)
+        else:
+            items = listing.objects.filter(isActive=False)
+        
         all_cat = Category.objects.all()
-        items = listing.objects.filter(isActive=False, category=c)
         return render(request, 'auctions/solditems.html', {
             "items" : items,
             "cat" : all_cat
